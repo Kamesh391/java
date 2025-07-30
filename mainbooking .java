f9b52786d2894a1c9d14efa7aa2a33a1cd852093
Kamesh391 <kameshwaranj84@gmail.com>
2025-07-30 04:08:54 -0700

    mainbooking .java

--- mainbooking .java
+++ mainbooking .java
@@ -94,3 +94,29 @@ public class MainBooking {
             System.out.println("Regret to book");
         }
     }
+    private static void allocateBerth(Passenger p) {
+        switch(p.getPrefBerth()){
+            case UPPER:
+                if(upper>0) {
+                    p.setAllocatedBerth(Berth.UPPER);
+                    upper--;
+                }
+            case LOWER:
+                if(p.getAllocatedBerth()==null && lower>0){
+                    p.setAllocatedBerth(Berth.LOWER);
+                    lower--;
+                }
+            case MIDDLE:
+                if(p.getAllocatedBerth()==null && middle>0){
+                    p.setAllocatedBerth(Berth.MIDDLE);
+                    middle--;
+                }else if(p.getAllocatedBerth()==null && upper>0){
+                    p.setAllocatedBerth(Berth.UPPER);
+                    upper--;
+                }else if(p.getAllocatedBerth()==null && lower>0){
+                    p.setAllocatedBerth(Berth.LOWER);
+                    lower--;
+                }
+        }
+    }
+}
